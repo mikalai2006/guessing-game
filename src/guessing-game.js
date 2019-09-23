65db25c2be422b6ee4b627ce66505cc21487276c
mikalai2006 <composer.opencart@gmail.com>
2019-09-23 22:15:53 +0300

    feat: add code in class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.numb = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
 
+        this.numb = Math.ceil(this.min + ((this.max - this.min) / 2));
+        // console.log(this)
+        return this.numb;
     }
 
     lower() {
-
+        this.max = this.numb;
     }
 
     greater() {
-
+        this.min = this.numb;
     }
 }
 
